5384da63b0915edf5bf1cbda9fd8174755262f39
flan02 <chanivetdan@hotmail.com>
2025-09-22 00:55:34 -0300

    Unit 3 theory added

--- hugging-face-mcp-course/core-implementation/client.py
+++ hugging-face-mcp-course/core-implementation/client.py
@@ -1,18 +1,32 @@
 import gradio as gr
 import os
-from mcp import StdioServerParameters
-from smolagents import InferenceClientModel, CodeAgent, ToolCollection, MCPClient
-from smolagents.mcp_client import MCPClient
 
-# with MCPClient(
-#     {"url": "https://abidlabs-mcp-tool-http.hf.space/gradio_api/mcp/sse"}
-# ) as tools:
-#     # Tools from the remote server are available
-#     print("\n".join(f"{t.name}: {t.description}" for t in tools))
+from smolagents import InferenceClientModel, CodeAgent, MCPClient
 
-mcp_client = MCPClient(
-    {
-        "url": "https://abidlabs-mcp-tool-http.hf.space/gradio_api/mcp/sse"
-    }  # This is the MCP Client we created in the previous section
-)
-tools = mcp_client.get_tools()
+mcp_client = None
+
+try:
+    mcp_client = MCPClient(
+        {"url": "https://abidlabs-mcp-tool-http.hf.space/gradio_api/mcp/sse"}
+    )
+    tools = mcp_client.get_tools()
+
+    model = InferenceClientModel(token=os.getenv("HUGGINGFACE_API_TOKEN"))
+    agent = CodeAgent(
+        tools=[*tools],
+        model=model,
+        additional_authorized_imports=["json", "ast", "urllib", "base64"],
+    )
+
+    demo = gr.ChatInterface(
+        fn=lambda message, history: str(agent.run(message)),
+        type="messages",
+        examples=["Analyze the sentiment of the following text 'This is awesome'"],
+        title="Agent with MCP Tools",
+        description="This is a simple agent that uses MCP tools to answer questions.",
+    )
+
+    demo.launch()
+finally:
+    if mcp_client:  # <-- chequea que exista
+        mcp_client.disconnect()
